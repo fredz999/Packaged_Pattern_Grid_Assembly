1d6e6867a943f5281c457727585ccd3c91f68646
fredz999 <jjjj58@hotmail.com>
2022-12-09 14:49:36 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -13,21 +13,21 @@ public class ComponentDimensions {
     public let backGroundWidth : CGFloat = 375
     public let backGroundHeight : CGFloat = 647
     
-    //============== Y SLIDER SECTION =======================
+
     public let ui_Unit_Width : CGFloat = 30
     public let ui_Unit_Height : CGFloat = 30
-    
-    public let pattern_Grid_Unit_Width : CGFloat = 16
-    public let pattern_Grid_Unit_Height : CGFloat = 16
     
     public let pattern_Grid_Unit_VerticalEdge_Width : CGFloat = 1
     public let pattern_Grid_Unit_HorzEdge_Width : CGFloat = 1
     
-    public let dataGrid_X_Unit_Count : Int = 16
-    public let DATA_final_Line_Y_Index : Int = 64
+    //============== Central Grid SECTION =========================
+    public let pattern_Grid_Unit_Width : CGFloat = 14
+    public let pattern_Grid_Unit_Height : CGFloat = 14
     
-    public let visualGrid_Y_Unit_Count : Int = 16
-    public let visualGrid_X_Unit_Count : Int = 16
+    public let DATA_final_Line_Y_Index : Int = 64
+    public let dataGrid_X_Unit_Count : Int = 22
+    public let visualGrid_Y_Unit_Count : Int = 22
+    public let visualGrid_X_Unit_Count : Int = 22
     
     public let cellFontSize : CGFloat = 12
     //============== CURSOR SECTION =========================
