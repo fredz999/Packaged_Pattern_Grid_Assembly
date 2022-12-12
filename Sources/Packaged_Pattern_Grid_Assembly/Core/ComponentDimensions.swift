699fb92835c01cd731055c914151dd4ac6974070
fredz999 <jjjj58@hotmail.com>
2022-12-12 11:18:38 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -23,11 +23,11 @@ public class ComponentDimensions : ObservableObject {
     //============== Central Grid SECTION =========================
     public let DATA_final_Line_Y_Index : Int = 64
     
-    public var pattern_Grid_Unit_Width : CGFloat = 16  // 22 or 16
-    public var pattern_Grid_Unit_Height : CGFloat = 16 // 22 or 16
+    public var pattern_Grid_Unit_Width : CGFloat = 22  // 22 or 16
+    public var pattern_Grid_Unit_Height : CGFloat = 22 // 22 or 16
     
-    public var dataGrid_X_Unit_Count : Int = 22 // 16 or 22
-    public var visualGrid_X_Unit_Count : Int = 22 // 16 or 22
+    public var dataGrid_X_Unit_Count : Int = 16 // 16 or 22
+    public var visualGrid_X_Unit_Count : Int = 16 // 16 or 22
     
     public let visualGrid_Y_Unit_Count : Int = 12
     public let cellFontSize : CGFloat = 12
