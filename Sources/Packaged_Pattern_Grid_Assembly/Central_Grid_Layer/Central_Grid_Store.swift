d49203e9faffc60625f38117cd44b7ef062d1059
fredz999 <jjjj58@hotmail.com>
2022-12-19 19:09:55 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Central_Grid_Store.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Central_Grid_Store.swift
@@ -163,7 +163,7 @@ public class Central_Cell_Store : ObservableObject,Identifiable {
      if referenced_dataCell_X_Number != newXNum{referenced_dataCell_X_Number = newXNum}
      if referenced_dataCell_Y_Number != newYNum{referenced_dataCell_Y_Number = newYNum}
      if referenced_isHighlighted != newHighlightedStatus{referenced_isHighlighted = newHighlightedStatus}
-     if referenced_currentStatus != newCellStatus{referenced_currentStatus = newCellStatus}
+     //if referenced_currentStatus != newCellStatus{referenced_currentStatus = newCellStatus}
 
 //     if let lclCurrentNote = referenced_note_Im_In {
 //         if let lclNewNote = newNoteImIn {
