f6d9a125d535f225fd8bb787000cda41255e7d0d
fredz999 <jjjj58@hotmail.com>
2022-12-21 14:07:21 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -28,7 +28,7 @@ public class ComponentDimensions : ObservableObject {
     @Published public var pattern_Grid_Unit_Width : CGFloat = 24  // 24 or 16
     @Published public var pattern_Grid_Unit_Height : CGFloat = 16 // 24 or 16
     
-    @Published public var dataGrid_X_Unit_Count : Int = 4
+    @Published public var dataGrid_X_Unit_Count : Int = 12
     public let visualGrid_Y_Unit_Count : Int = 1
     
     public let cellFontSize : CGFloat = 12
