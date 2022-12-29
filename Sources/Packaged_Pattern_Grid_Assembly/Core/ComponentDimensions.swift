1b7bc80a0485de35f1737784357e4b6ad61c4fcc
fredz999 <jjjj58@hotmail.com>
2022-12-29 14:46:47 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -71,13 +71,17 @@ public class ComponentDimensions : ObservableObject {
         
         if patternTimingConfiguration == .fourFour {
             patternTimingConfiguration = .sixEight
-            pattern_Grid_Cell_Sub_Unit_Count = 3
-            cursor_X_Jump = 24
+            pattern_Grid_Cell_Sub_Unit_Count = 2
+            cursor_X_Jump = 16
+//            pattern_Grid_Cell_Sub_Unit_Count = 3
+//            cursor_X_Jump = 24
         }
         else if patternTimingConfiguration == .sixEight {
             patternTimingConfiguration = .fourFour
-            pattern_Grid_Cell_Sub_Unit_Count = 2
-            cursor_X_Jump = 16
+            pattern_Grid_Cell_Sub_Unit_Count = 3
+            cursor_X_Jump = 24
+//            pattern_Grid_Cell_Sub_Unit_Count = 2
+//            cursor_X_Jump = 16
         }
 
     }
