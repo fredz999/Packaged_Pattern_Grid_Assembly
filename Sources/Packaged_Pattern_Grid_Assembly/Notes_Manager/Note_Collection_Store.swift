1dcce6f078adfac2fac4fb5d68dc454cb925415c
fredz999 <jjjj58@hotmail.com>
2022-12-29 15:31:07 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note_Collection_Store.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note_Collection_Store.swift
@@ -127,6 +127,7 @@ public class Note_Collection {
     }
     
     public func deleteNote(){
+        print("deleteNote(")
         if let lclCurrHighlighted = currentHighlightedNote{
             lclCurrHighlighted.resetCells()
             noteArray = noteArray.filter{$0.id == lclCurrHighlighted.id}
