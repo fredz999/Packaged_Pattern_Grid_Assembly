6a5aaf0aff385a097eaed386ea974cb21177c97c
fredz999 <jjjj58@hotmail.com>
2022-12-29 18:57:29 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note.swift
@@ -26,7 +26,7 @@ public class Note : ObservableObject, Identifiable, Equatable {
             if note_Highlighted == true {
                 for dataCell in dataCellArray {
                     dataCell.change_Highlight(highlightStatusParam: true)
-                        //.isHighlighted = true
+                    //.isHighlighted = true
                     
                     let vis_Y_Number = note_Y_Number - central_State.lower_Bracket_Number
                
