744b7e79ddd1ccecb0c98810bfbf4dff63e43b1e
fredz999 <jjjj58@hotmail.com>
2022-12-30 22:43:21 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Potential_Note_Layer_Store/Potential_Note_Layer_Store.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Potential_Note_Layer_Store/Potential_Note_Layer_Store.swift
@@ -52,12 +52,12 @@ public class Potential_Note_Layer_Store : ObservableObject {
             
             if lclInitialX <= currX {
                 x_Offset = dimensions.cursor_X_Jump * CGFloat(lclInitialX)
-                weeedth = (dimensions.cursor_X_Jump * CGFloat(currX+dimensions.cellArray_Write_Addition)) - dimensions.cursor_X_Jump * CGFloat(lclInitialX)
+                weeedth = (dimensions.cursor_X_Jump * CGFloat(currX+1)) - dimensions.cursor_X_Jump * CGFloat(lclInitialX)
                 y_Offset = dimensions.cursor_Y_Jump * CGFloat(initialY)
             }
             else if lclInitialX > currX {
                 x_Offset = dimensions.cursor_X_Jump * CGFloat(currX)
-                weeedth = (dimensions.cursor_X_Jump * CGFloat(lclInitialX+dimensions.cellArray_Write_Addition)) - dimensions.cursor_X_Jump * CGFloat(currX)
+                weeedth = (dimensions.cursor_X_Jump * CGFloat(lclInitialX+1)) - dimensions.cursor_X_Jump * CGFloat(currX)
                 y_Offset = dimensions.cursor_Y_Jump * CGFloat(initialY)
             }
             
