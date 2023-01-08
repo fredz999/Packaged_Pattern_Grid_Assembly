54381d87e08f3fb71995913c5ffbb20218b985a1
fredz999 <jjjj58@hotmail.com>
2023-01-08 17:38:51 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Data_Vals_Holder.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Data_Vals_Holder.swift
@@ -13,7 +13,11 @@ public class Data_Vals_Holder : ObservableObject {
    @Published public var referenced_dataCell_Y_Number : Int
    @Published public var referenced_isHighlighted : Bool = false
    @Published public var referenced_isProhibited : Bool = false
-   @Published public var referenced_currentStatus : E_CellStatus
+    @Published public var referenced_currentStatus : E_CellStatus{
+        didSet{
+            print("referenced_currentStatus: ",referenced_currentStatus.rawValue)
+        }
+    }
     
    public var referenced_note_Im_In : Note?
    
