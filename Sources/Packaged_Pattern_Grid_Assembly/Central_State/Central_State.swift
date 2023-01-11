4a040d737facce6044caa8564cd9c4bb43efdffc
fredz999 <jjjj58@hotmail.com>
2023-01-11 13:09:39 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -192,7 +192,7 @@ public class Central_State : ObservableObject {
             }
             else if nearestNoteRight == nil, nearestNoteLeft == nil {
                 if let lclCursorLayer = cursor_Layer_Ref {
-                    lclCursorLayer.setViableRegionMarker(lowerXParam: 0, upperXParam: 0)
+                    lclCursorLayer.setViableRegionMarker(lowerXParam: 0, upperXParam: 384)
                 }
             }
 
