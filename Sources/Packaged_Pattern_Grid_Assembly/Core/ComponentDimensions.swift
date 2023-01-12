d7b7688f06b376811013f25c461b98925565dc49
fredz999 <jjjj58@hotmail.com>
2023-01-12 16:23:08 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -131,7 +131,7 @@ public class ComponentColors {
     
     public var grid_Blank_Color : Color = Color(red:0.6,green:0,blue:0)
     public var viable_Set_Color : Color = Color(red:0,green:0.6,blue:0.6)
-    public var prohibited_Cell_Color : Color = Color(red:0.7,green:0,blue:0)
+    public var prohibited_Cell_Color : Color = Color(red:1,green:0.8,blue:0)
     public var grid_Note_Color : Color = Color(red:0.4,green:0,blue:0)
     public var grid_Note_Highlighted_Color : Color = Color(red:0.4,green:0.8,blue:0)
     
