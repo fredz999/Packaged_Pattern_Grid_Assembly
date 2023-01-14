5f8328f23036fc230ab7a7f526d246c139c7e8ce
fredz999 <jjjj58@hotmail.com>
2023-01-14 14:34:57 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -204,14 +204,15 @@ public class Central_State : ObservableObject {
             let viableCellsOnRight = cell_Line_Set.filter{$0.note_Im_In != nil && $0.dataCell_X_Number > currentData.dataCell_X_Number}
             let viableCellsOnLeft = cell_Line_Set.filter{$0.note_Im_In != nil && $0.dataCell_X_Number < currentData.dataCell_X_Number}
             var viableCells = Set<Underlying_Data_Cell>()
+            
+            viableCells = viableCellsOnRight.union(viableCellsOnLeft)
             viableCells.insert(currentData)
-            viableCells = viableCells.union(viableCellsOnRight)
-            viableCells = viableCells.union(viableCellsOnLeft)
             //if let lclViableSet = viableCells{
                 for dataCell in viableCells{
                     dataCell.change_Viable_Set_Status(viableSetMembershipParam:true)
                 }
             //}
+            print("viableCells.count: ",viableCells.count)
         }
 
     }
