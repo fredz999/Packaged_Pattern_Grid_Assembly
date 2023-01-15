b4f8f4ed7365f1837abac4c5daaec08fe27592f8
fredz999 <jjjj58@hotmail.com>
2023-01-15 16:38:35 +0000

    __

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -167,12 +167,12 @@ public class Central_State : ObservableObject {
                 }
             }
             else if let lclNewval = newValue,let previousViableSet = viableSet {
-            print("New Set Count: ",lclNewval.count.description,",previousViableSet count: ",previousViableSet.count.description)
-//            let resettables = lclNewval.subtracting(previousViableSet)
-//                for cell in resettables{
-//                    if cell.in_Viable_Set == true{cell.in_Viable_Set = false}
-//                    print("cellX: ",cell.dataCell_X_Number,", got set to not in viable")
-//                }
+            //print("New Set Count: ",lclNewval.count.description,",previousViableSet count: ",previousViableSet.count.description)
+            let resettables = lclNewval.subtracting(previousViableSet)
+                for cell in resettables{
+                    if cell.in_Viable_Set == true{cell.in_Viable_Set = false}
+                    print("resetting cell: ",cell.dataCell_X_Number," to not in viable set")
+                }
             }
         }
         didSet {
