af66d166a3762aa887a4b3756d982e172d05652e
fredz999 <jjjj58@hotmail.com>
2023-01-22 15:11:31 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -163,7 +163,11 @@ public class Central_State : ObservableObject {
         }
     }
     
+    var viableSetHelpers : Viable_Set_Helper_Functions
+    
     public init(){
+        currentXCursor_Slider_Position = 0
+        curr_Data_Pos_Y = 0
         viableSetHelpers = Viable_Set_Helper_Functions()
         viableSetHelpers.currentData = data_Grid.dataLineArray[0].dataCellArray[0]
         
@@ -172,8 +176,6 @@ public class Central_State : ObservableObject {
             viableSetHelpers.current_Cell_Line_Set.insert(cell)
         }
         
-        currentXCursor_Slider_Position = 0
-        curr_Data_Pos_Y = 0
     }
     
 //    func post_Init_Setup(){
@@ -195,7 +197,7 @@ public class Central_State : ObservableObject {
         }
     }
 
-    var viableSetHelpers = Viable_Set_Helper_Functions()
+    
     
     
     
