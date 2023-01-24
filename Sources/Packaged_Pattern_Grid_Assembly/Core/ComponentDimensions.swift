280304c73517152165f06b7152578eb8e2b78c3d
fredz999 <jjjj58@hotmail.com>
2023-01-24 11:40:39 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -122,15 +122,14 @@ public class ComponentColors {
     public var writeButtonFontColor : Color = Color(red: 1, green: 1, blue: 1)
     // ================================================================================
     // ================================================================================
-    public var grid_Blank_Color : Color = Color(red:0,green:0.5,blue:0.3)
-    public var viable_Set_Combined_Color : Color = Color(red:0.4,green:0.8,blue:0)
-    
-    
+
     public var prohibited_Cell_Color : Color = Color(red:0.8,green:0,blue:0)
     public var potential_Cell_Color : Color = Color(red:1,green:0,blue:1)
     
-    public var grid_Note_Color : Color = Color(red:0,green:0,blue:0)
+    public var grid_Note_Color : Color = Color(red:0.4,green:0.8,blue:0)
     public var grid_Note_Highlighted_Color : Color = Color(red:0.5,green:1,blue:0)
+    public var viable_Set_Combined_Color : Color = Color(red:0.3,green:0.6,blue:0)
+    public var grid_Blank_Color : Color = Color(red:0.2,green:0.4,blue:0)
     
     public static let StaticColors = ComponentColors()
 }
