00d32ffaed515287fdcfee1fc96190d74e588e8a
fredz999 <jjjj58@hotmail.com>
2023-01-31 15:17:16 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Cursor_Sliders_Layer/Cursor_Horizontal_Slider.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Cursor_Sliders_Layer/Cursor_Horizontal_Slider.swift
@@ -40,7 +40,6 @@ public class Cursor_Horizontal_Slider_Store : ObservableObject {
     
     public func handleDrag(inputParam:CGFloat){
 
-        print("handleDrag inputParam: ",inputParam.description)
         
         if (inputParam + accumulatedDrag) <= (dimensions.potential_Note_Rightward_Boundary),(inputParam + accumulatedDrag) >= dimensions.potential_Note_Leftward_Boundary {
             currentVal = inputParam + accumulatedDrag
