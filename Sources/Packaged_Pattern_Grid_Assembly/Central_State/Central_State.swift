edcefa1f8a3a460abbdf43c1d06d2638041eac1e
fredz999 <jjjj58@hotmail.com>
2023-02-02 22:56:01 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -116,11 +116,11 @@ public class Central_State : ObservableObject {
         
         
     
-        
+    dimensions.flip_Timing_Signature_Dimensions()
     data_Grid.changeTimingSignature_Data_Level()
 
     if dimensions.patternTimingConfiguration == .sixEight {
-        print("currdata six_Eight_Half_Sub_Index : ",viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index.description)
+                print("currdata six_Eight_Half_Sub_Index : ",viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index.description)
         if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 1 {
             if let lclH_SliderRef = h_Slider_Ref {
                 lclH_SliderRef.artificially_H_Decrement(numberOfCells: 1)
@@ -132,7 +132,7 @@ public class Central_State : ObservableObject {
         }
     }
     
-    dimensions.flip_Timing_Signature_Dimensions()
+    
 
     
         
