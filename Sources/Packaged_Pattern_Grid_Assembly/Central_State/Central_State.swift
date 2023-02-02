6bbd0bad2b15ba33951df746b07ccfa5f8cf8c29
fredz999 <jjjj58@hotmail.com>
2023-02-02 11:18:09 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -88,8 +88,6 @@ public class Central_State : ObservableObject {
     data_Grid.changeTimingSignature_Data_Level()
         
     print("timing switched to: ",dimensions.patternTimingConfiguration == .fourFour ? "4:4" : "6:8"
-    ,"currentXCursor_Slider_Position: ", currentXCursor_Slider_Position.description
-    ,", currCell dataCell_X_Number: ",viableSetHelpers.helperFuncs_currentData.dataCell_X_Number.description
     ,"cell six_Eight_Half_Sub_Index: ",viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index.description
     ,"cell four_Four_Half_Sub_Index: ",viableSetHelpers.helperFuncs_currentData.four_Four_Half_Sub_Index  .description)
         
