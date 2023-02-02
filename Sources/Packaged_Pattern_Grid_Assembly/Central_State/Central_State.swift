6320ae50ac092d98d097162ea21911bb946bca4b
fredz999 <jjjj58@hotmail.com>
2023-02-02 14:04:53 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -102,21 +102,22 @@ public class Central_State : ObservableObject {
 //            }
         }
         else if dimensions.patternTimingConfiguration == .sixEight{
-            if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 0{
-                print("set to 6:8 not moving")
-            }
-            else if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 1{
-                print("set to 6:8 moving back one")
-//                if let lclH_SliderRef = h_Slider_Ref{
-//                    lclH_SliderRef.artificially_H_Decrement(numberOfCells: 1)
-//                }
-            }
-            else if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 2{
-                print("set to 6:8 moving back two?")
-//                if let lclH_SliderRef = h_Slider_Ref{
-//                    lclH_SliderRef.artificially_H_Decrement(numberOfCells: 2)
-//                }
-            }
+            print("set to 6:8, sub half index: ",viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index.description)
+//            if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 0{
+//                print("set to 6:8 not moving")
+//            }
+//            else if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 1{
+//                print("set to 6:8 moving back one")
+////                if let lclH_SliderRef = h_Slider_Ref{
+////                    lclH_SliderRef.artificially_H_Decrement(numberOfCells: 1)
+////                }
+//            }
+//            else if viableSetHelpers.helperFuncs_currentData.six_Eight_Half_Sub_Index == 2{
+//                print("set to 6:8 moving back two?")
+////                if let lclH_SliderRef = h_Slider_Ref{
+////                    lclH_SliderRef.artificially_H_Decrement(numberOfCells: 2)
+////                }
+//            }
         }
         
         
