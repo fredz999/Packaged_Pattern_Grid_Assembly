189b42fd87fd2f26ed731de2d66d3b20b1ebb882
fredz999 <jjjj58@hotmail.com>
2023-02-02 18:57:58 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Cursor_Sliders_Layer/Cursor_Horizontal_Slider.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Cursor_Sliders_Layer/Cursor_Horizontal_Slider.swift
@@ -68,10 +68,10 @@ public class Cursor_Horizontal_Slider_Store : ObservableObject {
     }
 
     public func artificially_H_Decrement(numberOfCells:Int){
-        print("artificially_H_Decrement(numberOfCells: ",numberOfCells.description)
+        
         let movement = dimensions.pattern_Grid_Sub_Cell_Width * CGFloat(numberOfCells)
         accumulatedDrag -= movement //dimensions.cursor_X_Jump
-        
+        print("artificially_H_Decrement movement: ",movement.description)
         if (accumulatedDrag) <= (dimensions.initial_Right_Boundary),accumulatedDrag >= dimensions.initial_Left_Boundary {
             currentVal = accumulatedDrag
         }
