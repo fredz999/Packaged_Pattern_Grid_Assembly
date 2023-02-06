eddba82f0ce127fb4f535f189406318ef7cf5351
fredz999 <jjjj58@hotmail.com>
2023-02-06 15:12:51 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Grid.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Grid.swift
@@ -70,7 +70,7 @@ public class Underlying_Data_Grid:ObservableObject,Identifiable {
                     
                 }
                 
-                
+                print("6:8 halfSub: ",sixEight_Half_Sub_Count.description,", 4:4 halfSub: ",fourFour_Half_Sub_Count.description)
                 let newDataCell = Underlying_Data_Cell(xNumParam: x, yNumParam: y, fourStatusParam: currFourStatus, sixStatusParam: currSixStatus
                 , initialStatusParam: initialStatus, fourFourSubIndexParam: fourFour_Sub_Count, sixEightSubIndexParam: sixEight_Sub_Count
                 , four_Four_Cell_Index_Param: fourFour_Cell_Count, six_Eight_Cell_Index_Param: sixEight_Cell_Count
@@ -103,7 +103,7 @@ public class Underlying_Data_Grid:ObservableObject,Identifiable {
                     sixEight_Half_Sub_Count=0
                     sixEight_Half_Cell_Count+=1
                 }
-
+                
                 newLine.dataCellArray.append(newDataCell)
             }
             dataLineArray.append(newLine)
