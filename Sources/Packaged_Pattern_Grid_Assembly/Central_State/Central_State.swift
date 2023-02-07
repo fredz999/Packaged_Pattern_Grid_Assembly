506743ef6629b5090e0712e7b6ca0b1e638cdedd
fredz999 <jjjj58@hotmail.com>
2023-02-07 12:50:43 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -90,41 +90,31 @@ public class Central_State : ObservableObject {
     
     public func change_Timing_Signature_Central(){
 
-        
-//    if dimensions.patternTimingConfiguration == .sixEight{
-//        let fourSub = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[curr_Data_Pos_X].four_Four_Half_Sub_Index
-//        if fourSub == 1{
-//            if let lclHslider = h_Slider_Ref{
-//                lclHslider.jumpToACell(cellNum: (curr_Data_Pos_X-1))
-//            }
-//        }
-//        else if fourSub == 2{
-//            if let lclHslider = h_Slider_Ref{
-//                lclHslider.jumpToACell(cellNum: (curr_Data_Pos_X-2))
-//            }
-//        }
-//    }
-//    else if dimensions.patternTimingConfiguration == .fourFour{
-//        let sixSub = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[curr_Data_Pos_X].six_Eight_Half_Sub_Index
-//        if sixSub == 1{
-//            if let lclHslider = h_Slider_Ref{
-//                lclHslider.jumpToACell(cellNum: (curr_Data_Pos_X-1))
-//            }
-//        }
-//    }
-    
-        
     dimensions.flip_Timing_Signature_Dimensions()
     data_Grid.changeTimingSignature_Data_Level()
         
-    let fourSub = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[curr_Data_Pos_X].four_Four_Half_Sub_Index
-    let targetCellIndex = curr_Data_Pos_X-1
-    print("pre jump currX: ",curr_Data_Pos_X.description, "curr 44 half sub: ",fourSub.description,", targetCellIndex: ",targetCellIndex.description)
-    if let lclHslider = h_Slider_Ref{lclHslider.jumpToACell(cellNum: targetCellIndex )}
-    print("post jump currX: ",curr_Data_Pos_X.description, "curr 44 half sub: ",fourSub.description)
-        
-        
-        
+        if dimensions.patternTimingConfiguration == .sixEight{
+            let fourSub = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[curr_Data_Pos_X].four_Four_Half_Sub_Index
+            if fourSub == 1{
+                if let lclHslider = h_Slider_Ref{
+                    lclHslider.jumpToACell(cellNum: (curr_Data_Pos_X-1))
+                }
+            }
+            else if fourSub == 2{
+                if let lclHslider = h_Slider_Ref{
+                    lclHslider.jumpToACell(cellNum: (curr_Data_Pos_X-2))
+                }
+            }
+        }
+        else if dimensions.patternTimingConfiguration == .fourFour{
+            let sixSub = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[curr_Data_Pos_X].six_Eight_Half_Sub_Index
+            if sixSub == 1{
+                if let lclHslider = h_Slider_Ref{
+                    lclHslider.jumpToACell(cellNum: (curr_Data_Pos_X-1))
+                }
+            }
+        }
+
     }
 
     public func deleteANote(){
