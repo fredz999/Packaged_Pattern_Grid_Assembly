5b809da3a83f6137545f8d69def88367dc60a414
fredz999 <jjjj58@hotmail.com>
2023-02-10 15:10:47 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -154,7 +154,20 @@ class Viable_Set_Helper_Functions{
                         viableSet_Combined.filter({$0.dataCell_X_Number >= min_Cell.dataCell_X_Number
                         && $0.dataCell_X_Number <= max_Cell.dataCell_X_Number})
                         
+                        //helperFuncs_PotentialNoteSet = swipeSet
+                        
                         let swipeSet_InNote = swipeSet.filter{$0.note_Im_In == nil}
+                        
+                        if swipeSet_InNote.count > 0 {
+                            if let lowestInANote = swipeSet_InNote.min(by: {$0.dataCell_X_Number < $1.dataCell_X_Number}){
+                                helperFuncs_PotentialNoteSet = swipeSet.filter{$0.dataCell_X_Number < lowestInANote.dataCell_X_Number}
+                                in_Swipe_Inviables = swipeSet.filter{$0.dataCell_X_Number > lowestInANote.dataCell_X_Number}
+                            }
+                        }
+                        else if swipeSet_InNote.count == 0 {
+                            helperFuncs_PotentialNoteSet = swipeSet
+                        }
+                        
                         
 //                        if let lowestInANote = swipeSet_InNote.min(by: {$0.dataCell_X_Number < $1.dataCell_X_Number}){
 //                            helperFuncs_PotentialNoteSet = swipeSet.filter{$0.dataCell_X_Number < lowestInANote.dataCell_X_Number}
@@ -163,7 +176,8 @@ class Viable_Set_Helper_Functions{
 //                        else {
 //                            helperFuncs_PotentialNoteSet = swipeSet
 //                        }
-                        helperFuncs_PotentialNoteSet = swipeSet
+                        
+                        
                         
                     }
                     
