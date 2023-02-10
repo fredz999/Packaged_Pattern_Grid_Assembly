f12eed1a07e8e5e45585f7ab6ccf24dc976fd03f
fredz999 <jjjj58@hotmail.com>
2023-02-10 23:20:57 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -158,10 +158,10 @@ class Viable_Set_Helper_Functions{
                         
                         let swipeSet_InNote = swipeSet.filter{$0.note_Im_In == nil}
                         
-                        if swipeSet_InNote.count == 0 {
-                            helperFuncs_PotentialNoteSet = swipeSet
-                        }
-                        else if swipeSet_InNote.count > 0 {
+//                        if swipeSet_InNote.count == 0 {
+//                            helperFuncs_PotentialNoteSet = swipeSet
+//                        }
+                        if swipeSet_InNote.count > 0 {
                             
                             if let lowestInANote = swipeSet_InNote.min(by: {$0.dataCell_X_Number < $1.dataCell_X_Number}){
                                 
