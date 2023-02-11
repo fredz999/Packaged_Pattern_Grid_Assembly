a83bf363f1c2ce855c456d62a37618d7785b6a0c
fredz999 <jjjj58@hotmail.com>
2023-02-11 12:53:17 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -119,7 +119,7 @@ class Viable_Set_Helper_Functions{
                 if lclInitialCell.dataCell_X_Number < helperFuncs_currentData.dataCell_X_Number {
                      
                     currentSwipeDirection = .rightward
-
+print("establish_Potential_Cells_Set()")
 //                    let lowerHalfCellSet = viableSet_Combined.filter({$0.four_Four_Half_Cell_Index == lclInitialCell.four_Four_Half_Cell_Index})
 //
 //                    let upperHalfCellSet = viableSet_Combined.filter({$0.four_Four_Half_Cell_Index == helperFuncs_currentData.four_Four_Half_Cell_Index-1})
