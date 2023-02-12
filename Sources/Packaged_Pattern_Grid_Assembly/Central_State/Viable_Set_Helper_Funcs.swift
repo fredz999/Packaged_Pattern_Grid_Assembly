25d12b7535fa5f7af314f785a0a85f5cea13a9c1
fredz999 <jjjj58@hotmail.com>
2023-02-12 13:20:11 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -158,11 +158,12 @@ class Viable_Set_Helper_Functions{
                             && $0.dataCell_X_Number <= lclMinIllegal.dataCell_X_Number
                         })
                         
-                        print("lowerHalfCellSet count: ",lowerHalfCellSet.count.description
-                              ,", upperHalfCellSet count: ",upperHalfCellSet.count.description
-                            ,", combinedSet length: ", combinedSet.count.description
-                              ,", max_Cell.dataCell_X_Number: ",max_Cell.dataCell_X_Number.description
-                              ,", helperFuncs_currentData X: ",helperFuncs_currentData.dataCell_X_Number.description)
+                        print(
+//                              "lowerHalfCellSet count: ",lowerHalfCellSet.count.description
+//                              ,", upperHalfCellSet count: ",upperHalfCellSet.count.description
+//                              ,", combinedSet length: ", combinedSet.count.description
+                              ", max_Cell.dataCell_X_Number: ",max_Cell.dataCell_X_Number.description
+                              ,", helperFuncs_currentData.four_Four_Half_Cell_Index-1: ",(helperFuncs_currentData.four_Four_Half_Cell_Index-1).description)
                         
                         helperFuncs_PotentialNoteSet = swipeSet
                     }
