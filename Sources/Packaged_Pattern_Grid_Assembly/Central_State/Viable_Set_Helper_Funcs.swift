ec564d2a2372b18daf0d47a08b6e88ed9be52acc
fredz999 <jjjj58@hotmail.com>
2023-02-16 13:42:06 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -58,15 +58,15 @@ class Viable_Set_Helper_Functions{
 //            }
 //        }
         didSet {
-            if Central_State.Static_Central_State.writingIsOn == false {
+            //if Central_State.Static_Central_State.writingIsOn == false {
 //
-//                var nillableNote : Note? = nil
-//                for cell in helperFuncs_Cursor_Set {
-//                    cell.handleVisibleStateChange(type: .activate_Cursor_Set)
-//                    if let lclNote = cell.note_Im_In {
-//                        nillableNote = lclNote
-//                    }
-//                }
+                var nillableNote : Note? = nil
+                for cell in helperFuncs_Cursor_Set {
+                    cell.handleVisibleStateChange(type: .activate_Cursor_Set)
+                    if let lclNote = cell.note_Im_In {
+                        nillableNote = lclNote
+                    }
+                }
 //
 //                if let lclNoteCollection = Central_State.Static_Central_State.note_Collection_Ref {
 //                    if let lclNillableNote = nillableNote {
@@ -77,7 +77,7 @@ class Viable_Set_Helper_Functions{
 //                    }
 //                }
 //
-            }
+            //}
         }
     }
 
