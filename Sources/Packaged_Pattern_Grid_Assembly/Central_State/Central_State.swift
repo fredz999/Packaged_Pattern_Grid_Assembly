de9fb711099df4e11eda3d81d8cc46dbb685f8ff
fredz999 <jjjj58@hotmail.com>
2023-02-16 23:25:10 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -121,7 +121,7 @@ public class Central_State : ObservableObject {
     
     public var delete_Note_Tap_Gesture : some Gesture {
         TapGesture(count: 1).onEnded({
-            
+            self.viableSetHelpers.nil_Cursor_Set()
             self.deleteANote()
             
             if self.writingIsOn {
