b00830e80c8402d377c4b3045bf20298e8653832
fredz999 <jjjj58@hotmail.com>
2023-02-17 13:55:23 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -59,12 +59,12 @@ class Viable_Set_Helper_Functions{
         }
         didSet {
             if Central_State.Static_Central_State.writingIsOn == false {
-                var nillableNote : Note? = nil
+                //var nillableNote : Note? = nil
                 for cell in helperFuncs_Cursor_Set {
                     cell.handleVisibleStateChange(type: .activate_Cursor_Set)
-                    if let lclNote = cell.note_Im_In {
-                        nillableNote = lclNote
-                    }
+//                    if let lclNote = cell.note_Im_In {
+//                        nillableNote = lclNote
+//                    }
                 }
 
 //                if let lclNoteCollection = Central_State.Static_Central_State.note_Collection_Ref {
