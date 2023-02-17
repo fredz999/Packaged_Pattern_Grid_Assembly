27d06481971636cb805fc52538646a658408edf4
fredz999 <jjjj58@hotmail.com>
2023-02-17 13:21:20 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Data_Vals_Holder.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Data_Vals_Holder.swift
@@ -66,6 +66,7 @@ public class Data_Vals_Holder : ObservableObject {
             }
         }
         else if status_Update_TypeParam == .cursorSet {
+            print("status_Update_TypeParam == .cursorSet ")
             if value == true {
                 if referenced_in_Cursor_Set == false {
                     referenced_in_Cursor_Set=true
@@ -77,6 +78,7 @@ public class Data_Vals_Holder : ObservableObject {
         }
         
         else if status_Update_TypeParam == .highlighted {
+            print("status_Update_TypeParam == .highlighted ")
             if value == true {
                 if referenced_in_Highlighted_Set == false{referenced_in_Highlighted_Set=true}
             }
