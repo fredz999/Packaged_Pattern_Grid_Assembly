a4014c49544e34aa1d94c3472a25094497e810f9
fredz999 <jjjj58@hotmail.com>
2023-02-17 09:03:51 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -121,17 +121,20 @@ public class Central_State : ObservableObject {
     
     public var delete_Note_Tap_Gesture : some Gesture {
         TapGesture(count: 1).onEnded({
-            self.viableSetHelpers.nil_Cursor_Set()
+            
             
             self.deleteANote()
-//            if self.writingIsOn {
-//                if let lclInitial = self.viableSetHelpers.initial_WriteOnCell{
-//                    let variableDelta = (self.viableSetHelpers.helperFuncs_currentData.dataCell_X_Number - lclInitial.dataCell_X_Number)
-//                    if variableDelta > self.viableSetHelpers.helperFuncs_PotentialNote_Set.count
-//                    || variableDelta < self.viableSetHelpers.helperFuncs_PotentialNote_Set.count{self.viableSetHelpers.establish_Potential_Cells_Set()}
-//                }
-//
-//            }
+            
+            self.viableSetHelpers.nil_Cursor_Set()
+            
+            if self.writingIsOn {
+                if let lclInitial = self.viableSetHelpers.initial_WriteOnCell{
+                    let variableDelta = (self.viableSetHelpers.helperFuncs_currentData.dataCell_X_Number - lclInitial.dataCell_X_Number)
+                    if variableDelta > self.viableSetHelpers.helperFuncs_PotentialNote_Set.count
+                    || variableDelta < self.viableSetHelpers.helperFuncs_PotentialNote_Set.count{self.viableSetHelpers.establish_Potential_Cells_Set()}
+                }
+
+            }
 //            else if self.writingIsOn == false {
 //                self.viableSetHelpers.establish_Cursor_Set()
 //            }
