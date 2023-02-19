fbde9ee152fc2ee9518b7cd6ed9e4276a786b277
fredz999 <jjjj58@hotmail.com>
2023-02-19 15:00:18 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -103,10 +103,14 @@ class Viable_Set_Helper_Functions{
 //            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.six_Eight_Half_Cell_Index == helperFuncs_currentData.six_Eight_Half_Cell_Index})
 //        }
         if dimensions.patternTimingConfiguration == .fourFour {
-            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number})
+            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
+                || $0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number+1
+            })
         }
         else if dimensions.patternTimingConfiguration == .sixEight {
-            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number})
+            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
+                || $0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number+1
+            })
         }
     }
     
