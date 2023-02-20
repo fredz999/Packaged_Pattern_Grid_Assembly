3546b63b9085c2d01cb7c75120f21e310b37a34a
fredz999 <jjjj58@hotmail.com>
2023-02-20 17:05:36 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Viable_Set_Helper_Funcs.swift
@@ -156,24 +156,23 @@ class Viable_Set_Helper_Functions{
 
     func establish_Cursor_Set(){
         
-//        if dimensions.patternTimingConfiguration == .fourFour {
-//            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.four_Four_Half_Cell_Index == helperFuncs_currentData.four_Four_Half_Cell_Index})
-//        }
-//        else if dimensions.patternTimingConfiguration == .sixEight {
-//            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.six_Eight_Half_Cell_Index == helperFuncs_currentData.six_Eight_Half_Cell_Index})
-//        }
-        
         if dimensions.patternTimingConfiguration == .fourFour {
-            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
-                || $0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
-            })
+            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.four_Four_Half_Cell_Index == helperFuncs_currentData.four_Four_Half_Cell_Index})
         }
         else if dimensions.patternTimingConfiguration == .sixEight {
-            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
-                || $0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
-            })
+            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.six_Eight_Half_Cell_Index == helperFuncs_currentData.six_Eight_Half_Cell_Index})
         }
         
+//        if dimensions.patternTimingConfiguration == .fourFour {
+//            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
+//                || $0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
+//            })
+//        }
+//        else if dimensions.patternTimingConfiguration == .sixEight {
+//            helperFuncs_Cursor_Set = current_Cell_Line_Set.filter({$0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
+//                || $0.dataCell_X_Number == helperFuncs_currentData.dataCell_X_Number
+//            })
+//        }
         
     }
     
