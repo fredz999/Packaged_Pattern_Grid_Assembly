5d53e9a07931ab66da7345cfef920e3e9bedc11f
fredz999 <jjjj58@hotmail.com>
2023-02-25 20:36:45 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Grid.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Grid.swift
@@ -190,6 +190,7 @@ public class Underlying_Data_Cell:Identifiable,Equatable,Hashable {
             if in_Delete_Square_Set == false {
                 in_Delete_Square_Set = true
                 if let lclDataVals = currentConnectedDataVals {
+                    print("activate del square set")
                     lclDataVals.update_Cell_Set_Membership(status_Update_TypeParam: .deleteSquareSet , value: in_Delete_Square_Set)
                 }
             }
