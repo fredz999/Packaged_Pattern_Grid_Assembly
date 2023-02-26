c099642aff47dee44c69e6c8d515a72f914e34ad
fredz999 <jjjj58@hotmail.com>
2023-02-26 09:52:55 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
@@ -153,18 +153,23 @@ class Delete_Helper {
             //if stationary, next move sets direction
             //after that the next move which is not the direction is the end of that line
             if current_Direction == .stationary {
-                if prevX != initialX{current_Direction = .horizontal}
-                else if prevY != initialY{current_Direction = .vertical}
+                if prevX != initialX{
+                    delete_Cursor_InitialData = previousDataCell
+                    current_Direction = .horizontal}
+                
+                else if prevY != initialY{
+                    delete_Cursor_InitialData = previousDataCell
+                    current_Direction = .vertical}
             }
             else if current_Direction == .horizontal {
                 if prevY != nextY{
-                    delete_Cursor_InitialData = previousDataCell
+                    
                     current_Direction = .stationary
                 }
             }
             else if current_Direction == .vertical {
                 if prevX != nextX{
-                    delete_Cursor_InitialData = previousDataCell
+                    
                     current_Direction = .stationary
                 }
             }
