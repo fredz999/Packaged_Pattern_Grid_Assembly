5ae5363dec9b4e8d826f24f8c65540fa2374d302
fredz999 <jjjj58@hotmail.com>
2023-02-27 10:38:35 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
@@ -191,7 +191,7 @@ class Delete_Helper {
                     Underlying_Data_Grid.Static_Underlying_Data_Grid.grid_Of_Cells_Set
                     .filter{
                         $0.dataCell_X_Number ==  initialX
-                        && $0.four_Four_Half_Cell_Index == nextFourFour
+                        //&& $0.four_Four_Half_Cell_Index == nextFourFour
                         && $0.dataCell_Y_Number > initialY
                         && $0.dataCell_Y_Number < nextY
                     }
