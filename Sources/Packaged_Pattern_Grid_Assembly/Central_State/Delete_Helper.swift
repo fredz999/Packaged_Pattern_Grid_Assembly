4e2dd4b54c6da2d531787f2c4207f15f7ce4736b
fredz999 <jjjj58@hotmail.com>
2023-02-27 12:39:15 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
@@ -186,20 +186,7 @@ class Delete_Helper {
             }
             else if current_Direction == .vertical {
                 
-                if nextY > initialY {
-                    let new_Vert_Set =
-                    Underlying_Data_Grid.Static_Underlying_Data_Grid.grid_Of_Cells_Set
-                    .filter{
-                    $0.dataCell_X_Number ==  nextX
-                    && $0.four_Four_Half_Cell_Index == nextFourFour
-//                    && $0.dataCell_Y_Number > initialY
-//                    && $0.dataCell_Y_Number <= nextY
-                    }
-                    for cell in new_Vert_Set{
-                        multiple_Line_Corners_Set.insert(cell)
-                    }
-                }
-                
+                incorporate_Column_Into_DeleteSet(curr_X:nextX,initialY:initialY,finalY:nextY)
                 
                 if prevX != nextX{
                     current_Trail_Corner = previousDataCell
@@ -209,6 +196,21 @@ class Delete_Helper {
             
         }
 
+    }
+    
+    func incorporate_Column_Into_DeleteSet(curr_X:Int,initialY:Int,finalY:Int){
+        if finalY > initialY {
+            let new_Vert_Set =
+            Underlying_Data_Grid.Static_Underlying_Data_Grid.grid_Of_Cells_Set
+            .filter{
+            $0.dataCell_X_Number ==  curr_X
+            && $0.dataCell_Y_Number > initialY
+            && $0.dataCell_Y_Number <= finalY
+            }
+            for cell in new_Vert_Set{
+                multiple_Line_Corners_Set.insert(cell)
+            }
+        }
     }
 
     func nil_Delete_Square_Set(){
