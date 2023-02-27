6e9423449612f0e263850fd095a02844111790d2
fredz999 <jjjj58@hotmail.com>
2023-02-27 10:50:37 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Delete_Helper.swift
@@ -190,10 +190,10 @@ class Delete_Helper {
                     let new_Vert_Set =
                     Underlying_Data_Grid.Static_Underlying_Data_Grid.grid_Of_Cells_Set
                     .filter{
-                        $0.dataCell_X_Number ==  nextX
-                        //&& $0.four_Four_Half_Cell_Index == nextFourFour
-                        && $0.dataCell_Y_Number > initialY
-                        && $0.dataCell_Y_Number < nextY
+                        //$0.dataCell_X_Number ==  nextX
+                    $0.four_Four_Half_Cell_Index == nextFourFour
+//                        && $0.dataCell_Y_Number > initialY
+//                        && $0.dataCell_Y_Number < nextY
                     }
                     for cell in new_Vert_Set{
                         multiple_Line_Corners_Set.insert(cell)
