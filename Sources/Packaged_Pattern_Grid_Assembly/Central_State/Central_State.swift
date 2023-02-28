ff575bfd3d8efe5de04187c1b13a601cb8c21499
fredz999 <jjjj58@hotmail.com>
2023-02-28 12:07:13 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -125,17 +125,17 @@ public class Central_State : ObservableObject {
         
         
         if patternModeParam == .writing {
-            if currentPatternMode != .writing {currentPatternMode = .writing}
-            delete_Helper.nil_Delete_Square_Set()
-            potential_Helper.nil_Cursor_Set()
-            if let lclMoveHelper = move_Helper{lclMoveHelper.nil_Move_Note_Cursor_Set()}
-            
-            if dimensions.patternTimingConfiguration == .fourFour {
-                potential_Helper.initial_WriteOnCell = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[dimensions.currentFourFourDataIndex]
-            }
-            else if dimensions.patternTimingConfiguration == .sixEight {
-                potential_Helper.initial_WriteOnCell = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[dimensions.currentSixEightDataIndex]
-            }
+//            if currentPatternMode != .writing {currentPatternMode = .writing}
+//            delete_Helper.nil_Delete_Square_Set()
+//            potential_Helper.nil_Cursor_Set()
+//            if let lclMoveHelper = move_Helper{lclMoveHelper.nil_Move_Note_Cursor_Set()}
+//            
+//            if dimensions.patternTimingConfiguration == .fourFour {
+//                potential_Helper.initial_WriteOnCell = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[dimensions.currentFourFourDataIndex]
+//            }
+//            else if dimensions.patternTimingConfiguration == .sixEight {
+//                potential_Helper.initial_WriteOnCell = data_Grid.dataLineArray[curr_Data_Pos_Y].dataCellArray[dimensions.currentSixEightDataIndex]
+//            }
             //potential_Helper.establish_Potential_Cells_Set()
         }
         
