10e551bc1a039aa897501b226fe64809db35cc6c
fredz999 <jjjj58@hotmail.com>
2023-03-03 21:31:12 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -62,7 +62,7 @@ class Move_Helper {
                     var tempCellSet = Set<Underlying_Data_Cell>()
                     //TODO: safe border limits
                     // ok, safe....deltaX
-                    if newStartX >= 0 {
+                    if newStartX >= 0,(newStartX+lcl_Captured_Cells.count) <= dimensions.dataGrid_X_Unit_Count {
                         for x in newStartX..<(newStartX+lcl_Captured_Cells.count){
                             tempCellSet.insert(dataGrid.dataLineArray[newStartY].dataCellArray[x])
                         }
