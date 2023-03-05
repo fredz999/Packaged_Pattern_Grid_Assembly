4412b99ebc518cc0d82676f4de60ad4ab5def7b6
fredz999 <jjjj58@hotmail.com>
2023-03-05 22:30:42 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -158,9 +158,9 @@ class Move_Helper {
     
     func writeMovedNote_DeleteOldNote(){
         if potential_Moved_Set.count > 0, let lclSnapshotNote = snapShot_Note_Id_Param {
+        note_Collection_Ref.delete_Current_Highlighted_Note(note_Id_Param:lclSnapshotNote)
         note_Collection_Ref.write_Note_Data(cellSetParam: potential_Moved_Set)
         nil_Cell_Sets()
-        note_Collection_Ref.delete_Current_Highlighted_Note(note_Id_Param:lclSnapshotNote)
         }
     }
     
