557f2b815767759336dec125825a5cae870b4572
fredz999 <jjjj58@hotmail.com>
2023-03-05 13:06:06 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -160,7 +160,8 @@ public class ComponentColors {
     public var passiveMode_Cursor_Color : Color = .pink
     public var deleteNoteMode_Cursor_Color : Color = Color(red:1,green:0.5,blue:0)
     
-    public var moveNoteMode_Cursor_Color : Color = Color(red:1,green:1,blue:1)
+    public var moveNoteMode_Cursor_Color : Color = Color(red:0.3,green:1,blue:1)
+    
     public var moveNote_Note_Color : Color = Color(red:1,green:1,blue:1)
 
     public var grid_Note_Color : Color = Color(red:0.4,green:0.8,blue:0)
