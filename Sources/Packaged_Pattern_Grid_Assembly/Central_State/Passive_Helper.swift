5c96e72088e3a2915f8df3d1f1ac4a157d748ae4
fredz999 <jjjj58@hotmail.com>
2023-03-07 17:10:09 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Passive_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Passive_Helper.swift
@@ -10,8 +10,8 @@ import Foundation
 class Passive_Helper: P_Selectable_Mode {
     
     let dimensions = ComponentDimensions.StaticDimensions
-//        
-//    let note_Collection_Ref = Note_Collection.Static_Note_Collection
+        
+    let note_Collection_Ref = Note_Collection.Static_Note_Collection
     
     var mode_Active: Bool = false
 
