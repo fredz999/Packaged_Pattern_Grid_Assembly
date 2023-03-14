fc9c7eb344c20b86d2569e04c335cc7d0d680cc2
fredz999 <jjjj58@hotmail.com>
2023-03-14 13:46:57 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note_Collection_Store.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note_Collection_Store.swift
@@ -51,7 +51,7 @@ public class Note_Collection {
     
     func accessSelected_Notes_Array(currentHighlightedNote : Note?){
         // a nil one is now going to empy the array
-        print("accessSelected_Notes_Array( note: ",currentHighlightedNote == nil ? "nil " : "not nil")
+        //print("accessSelected_Notes_Array( note: ",currentHighlightedNote == nil ? "nil " : "not nil")
         
         if let lclCurrentHighlightedNote = currentHighlightedNote {
             lclCurrentHighlightedNote.highlighted = true
@@ -77,6 +77,8 @@ public class Note_Collection {
                 selected_Notes_Array.removeAll()
             }
         }
+        
+        print("selected_Notes_Array count: ",selected_Notes_Array.count.description)
 
     }
     
