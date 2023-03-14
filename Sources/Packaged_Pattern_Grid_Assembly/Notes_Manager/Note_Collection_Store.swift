433fdbf8399d71db3d772e7f87b55ec506382b19
fredz999 <jjjj58@hotmail.com>
2023-03-14 14:41:09 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note_Collection_Store.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Notes_Manager/Note_Collection_Store.swift
@@ -54,9 +54,9 @@ public class Note_Collection {
         if let lclCurrentHighlightedNote = currentHighlightedNote {
             lclCurrentHighlightedNote.highlighted = true
             selected_Notes_Array.append(lclCurrentHighlightedNote)
-            if selected_Notes_Array.count == 2{
-                selected_Notes_Array.remove(at: 0)
-            }
+//            if selected_Notes_Array.count == 2{
+//                selected_Notes_Array.remove(at: 0)
+//            }
             //selected_Notes_Array.insert(lclCurrentHighlightedNote, at: 0)
             
             
