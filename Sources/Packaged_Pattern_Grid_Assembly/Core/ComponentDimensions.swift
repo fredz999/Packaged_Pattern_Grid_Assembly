51868adcb4d811caa3fca34dd17a37f056751b78
fredz999 <jjjj58@hotmail.com>
2023-03-16 20:32:15 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -76,51 +76,8 @@ public class ComponentDimensions : ObservableObject {
     
     var nearestLeftFourFourCellStartIndex : Int = 0
     var nearestLeftSixEightCellStartIndex : Int = 0
-    
-    
-//    func test_X_Position(currValParam:CGFloat,computedLineParam:inout CGFloat){
-//        
-//        let lesserSetFourFour = four_Four_Slider_Positions.filter{$0.x_Position_Float <= currValParam}
-//        
-//        if let lclMaxLesserFour = lesserSetFourFour.max(by:{$0.x_Position_Int < $1.x_Position_Int}) {
-//            if lclMaxLesserFour.x_Position_Float != currentFourFourPosition{currentFourFourPosition = lclMaxLesserFour.x_Position_Float}
-//            if lclMaxLesserFour.x_Position_Int != currentFourFourDataIndex{currentFourFourDataIndex = lclMaxLesserFour.x_Position_Int}
-//        }
-//        
-//        let lesserSetSixEight = six_Eight_Slider_Positions.filter{$0.x_Position_Float <= currValParam}
-//        
-//        if let lclMaxLesserSix = lesserSetSixEight.max(by:{$0.x_Position_Int < $1.x_Position_Int}) {
-//            if lclMaxLesserSix.x_Position_Float != currentSixEightPosition{currentSixEightPosition = lclMaxLesserSix.x_Position_Float}
-//            if lclMaxLesserSix.x_Position_Int != currentSixEightDataIndex{currentSixEightDataIndex = lclMaxLesserSix.x_Position_Int}
-//        }
-//        
-//        if patternTimingConfiguration == .fourFour {
-//            if computedLineParam != currentFourFourPosition{
-//                computedLineParam = currentFourFourPosition
-//                Central_State.Static_Central_State.cursor_Slider_Update()
-//            }
-//        }
-//        
-//        else if patternTimingConfiguration == .sixEight {
-//            if computedLineParam != currentSixEightPosition {
-//                computedLineParam = currentSixEightPosition
-//                Central_State.Static_Central_State.cursor_Slider_Update()
-//            }
-//        }
-//        
-//    }
-    
-//    func test_Y_Position(currValParam:CGFloat,computedLineParam:inout CGFloat){
-//        let divided = currValParam/pattern_Grid_Unit_Height
-//        let intDivided = Int(divided)
-//        if intDivided != Central_State.Static_Central_State.currentYCursor_Slider_Position {
-//            Central_State.Static_Central_State.currentYCursor_Slider_Position = intDivided
-//            Central_State.Static_Central_State.cursor_Slider_Update()
-//            computedLineParam = CGFloat(intDivided) * pattern_Grid_Unit_Height
-//        }
-//    }
 
-    public func flip_Timing_Signature_Dimensions(){
+    public func flip_Timing_Signature_Dimensions() {
         if patternTimingConfiguration == .fourFour {
             patternTimingConfiguration = .sixEight
             pattern_Grid_Cell_Sub_Unit_Count = 4
@@ -130,51 +87,41 @@ public class ComponentDimensions : ObservableObject {
             pattern_Grid_Cell_Sub_Unit_Count = 6
         }
     }
-    
-//    init(){
-//        Init_Tracker.Static_Init_Tracker.registerClass(classParam: .ComponentDimensions)
-//    }
-    
+
     public static let StaticDimensions = ComponentDimensions()
     
 }
 
 public class ComponentColors {
-    public var cursorSwiperColor : Color = Color(red:0.5,green:1,blue:0)
-    public var cursorSliderLineColor : Color = Color(red:0.5,green:1,blue:0)
     
-    //public var cursorNotWriting : Color = Color(red:0.4,green:0.8,blue:0)
-    //public var cursorWriting : Color = Color(red:0.5,green:1,blue:0)
+    public var cursorSwiperColor : Color = Color(red:0.5,green:1,blue:0)
+    
+    public var cursorSliderLineColor : Color = Color(red:0.5,green:1,blue:0)
     
     public var potentialColor : Color = Color(red: 0, green: 1, blue: 1)
     
-    //public var writeButtonColor : Color = Color(red: 0, green: 0.6, blue: 0)
+    public var multiSelectColor : Color = Color(red:0.3,green:0.5,blue:0.1)
     
-    //public var writeButtonFontColor : Color = Color(red: 1, green: 1, blue: 1)
-    // ================================================================================
-    // ================================================================================
     public var prohibited_Cell_Color : Color = Color(red:0.6,green:0,blue:0)
+    
     public var potential_Cell_Color : Color = Color(red:0.6,green:0,blue:0.6)
 
     public var passiveMode_Cursor_Color : Color = .pink
+    
     public var deleteNoteMode_Cursor_Color : Color = Color(red:1,green:0.5,blue:0)
     
     public var moveNoteMode_Cursor_Color : Color = Color(red:0.3,green:1,blue:1)
-    //public var moveNote_Note_Color : Color = Color(red:1,green:1,blue:1)
-    //public var grid_Note_Highlighted_Move_Color : Color = Color(red:1,green:1,blue:1)
-    
-    
 
     public var grid_Note_Color : Color = Color(red:0.4,green:0.8,blue:0)
     
     public var grid_Note_Highlighted_Color : Color = Color(red:0.5,green:1,blue:0)
     
-    
-    
     public var grid_Note_Highlighted_Resize_Color : Color = Color(red:0,green:1,blue:1)
     
     public var viable_Set_Combined_Color : Color = Color(red:0.3,green:0.6,blue:0)
+    
     public var grid_Blank_Color : Color = Color(red:0.2,green:0.4,blue:0)
     
     public static let StaticColors = ComponentColors()
+    
 }
