04ea776d6525a322001cb7fa645ed90cf741162f
fredz999 <jjjj58@hotmail.com>
2023-03-21 11:27:40 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -20,13 +20,16 @@ class Move_Helper: P_Selectable_Mode {
         if mode_Active == false {
             mode_Active = true
             if let lclActivationCell = activationCell{
+                
                 snapshot_Cursor_X = lclActivationCell.dataCell_X_Number
+                snapshot_Cursor_Y = lclActivationCell.dataCell_Y_Number
+                
                 if let lclCurrNoteCollection = parentCentralState.currentNoteCollection {
                     let selectedNotes = lclCurrNoteCollection.noteArray.filter{$0.highlighted == true}
                     
                     for selectedNote in selectedNotes {
                         let note_Movement_SnapShot = Note_Movement_SnapShot(note_Low_Index: selectedNote.lowest_Index
-                        , note_High_Index: selectedNote.highest_Index
+                        , note_High_Index: selectedNote.highest_Index, note_Y_Index_Param: selectedNote.note_Y_Number
                         , snapshotNoteIdParam: selectedNote.id)
                         //noteSnapShotArray.append(note_Movement_SnapShot)
                         let movingCellSetHolder = Moving_Cell_Set_Holder(initial_Snapshot_Param: note_Movement_SnapShot)
@@ -59,6 +62,7 @@ class Move_Helper: P_Selectable_Mode {
 //    var note_Low_Index : Int?
 //    var note_High_Index : Int?
     var snapshot_Cursor_X : Int?
+    var snapshot_Cursor_Y : Int?
     
 //    var note_Low_Index_Array : [Int] = []
 //    var note_High_Index_lowNote_Array : [Int] = []
@@ -147,15 +151,19 @@ class Move_Helper: P_Selectable_Mode {
         //for moving_Cell_Set in moving_Cell_Set_Holder_Array{
         for m in 0..<moving_Cell_Set_Holder_Array.count{
             var proposedSet = Set<Underlying_Data_Cell>()
-            if let lclSnapshot_X = snapshot_Cursor_X {
+            if let lclSnapshot_X = snapshot_Cursor_X,let lclSnapshot_Y = snapshot_Cursor_Y {
                 let delta_X_Grid_Units = parentCentralState.curr_Data_Pos_X - lclSnapshot_X
+                let delta_Y_Grid_Units = parentCentralState.curr_Data_Pos_Y - lclSnapshot_Y
                 //let proposedNewMinIndex = moving_Cell_Set.initial_Snapshot.note_Low_Index + delta_X_Grid_Units
+                
+                let proposedNewYNumber = moving_Cell_Set_Holder_Array[m].initial_Snapshot.note_Y_Index + delta_Y_Grid_Units
+                
                 let proposedNewMinIndex = moving_Cell_Set_Holder_Array[m].initial_Snapshot.note_Low_Index + delta_X_Grid_Units
                 //let proposedNewMaxIndex = moving_Cell_Set.initial_Snapshot.note_High_Index + delta_X_Grid_Units
                 let proposedNewMaxIndex = moving_Cell_Set_Holder_Array[m].initial_Snapshot.note_High_Index + delta_X_Grid_Units
 
                 let newLineSet : Set<Underlying_Data_Cell>
-                = Set(parentCentralState.data_Grid.dataLineArray[(parentCentralState.curr_Data_Pos_Y+m)].dataCellArray)
+                = Set(parentCentralState.data_Grid.dataLineArray[proposedNewYNumber].dataCellArray)
                 //parentCentralState.currLineSet
                 if proposedNewMinIndex >= currLeftLimit && proposedNewMaxIndex <= currRightLimit {
                     proposedSet = newLineSet
@@ -243,11 +251,13 @@ protocol P_Selectable_Mode {
 class Note_Movement_SnapShot{
     var note_Low_Index : Int
     var note_High_Index : Int
+    var note_Y_Index : Int
     var snapShot_Note_Id_Param : UUID
-    init(note_Low_Index: Int, note_High_Index: Int,snapshotNoteIdParam:UUID) {
+    init(note_Low_Index: Int, note_High_Index: Int,note_Y_Index_Param:Int,snapshotNoteIdParam:UUID) {
         self.note_Low_Index = note_Low_Index
         self.note_High_Index = note_High_Index
         snapShot_Note_Id_Param = snapshotNoteIdParam
+        note_Y_Index = note_Y_Index_Param
     }
 }
 
