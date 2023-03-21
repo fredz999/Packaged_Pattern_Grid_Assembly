fd62fbe7f3e4069fa2d695e5c9cf2d8cb575e185
fredz999 <jjjj58@hotmail.com>
2023-03-21 16:05:17 +0000

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -11,9 +11,7 @@ import SwiftUI
 class Move_Helper: P_Selectable_Mode {
     
     var mode_Active: Bool = false
-    
-    //var noteSnapShotArray : [Note_Movement_SnapShot] = []
-    
+
     var moving_Cell_Set_Holder_Array : [Moving_Cell_Set_Holder] = []
     
     func activate_Mode(activationCell: Underlying_Data_Cell?) {
@@ -52,58 +50,15 @@ class Move_Helper: P_Selectable_Mode {
     var parentCentralState : Central_State
     
     let dimensions = ComponentDimensions.StaticDimensions
-    
-    
-    
-    
-    
-    
-    
-//    var note_Low_Index : Int?
-//    var note_High_Index : Int?
+
     var snapshot_Cursor_X : Int?
     var snapshot_Cursor_Y : Int?
-    
-//    var note_Low_Index_Array : [Int] = []
-//    var note_High_Index_lowNote_Array : [Int] = []
-//    var snapshot_Cursor_X_Array : [Int] = []
-    //var snapShot_Note_Id_Param : UUID?
     
     var currLeftLimit : Int
     var currRightLimit : Int
     var lineBelowOpen : Bool
     var lineAboveOpen : Bool
-    
-//    var potential_Moved_Set = Set<Underlying_Data_Cell>(){
-//        willSet {
-//            let delta = potential_Moved_Set.symmetricDifference(newValue)
-//            for cell in delta {
-//                cell.handleVisibleStateChange(type: .deActivate_Potential_Set)
-//            }
-//        }
-//        didSet {
-//            for cell in potential_Moved_Set {
-//                cell.handleVisibleStateChange(type : .activate_Potential_Set)
-//            }
-//        }
-//    }
-//
-//    var prohibition_Indicator_Set = Set<Underlying_Data_Cell>(){
-//        willSet {
-//            let delta = prohibition_Indicator_Set.symmetricDifference(newValue)
-//            for cell in delta {
-//                cell.handleVisibleStateChange(type: .deActivate_Prohibited)
-//            }
-//        }
-//        didSet {
-//            for cell in prohibition_Indicator_Set {
-//                cell.handleVisibleStateChange(type : .activate_Prohibited)
-//            }
-//        }
-//    }
-    
-    //var proposedSet = Set<Underlying_Data_Cell>()
-    
+
     init(parentCentral_State_Param:Central_State){
         parentCentralState = parentCentral_State_Param
         currLeftLimit = 0
@@ -111,40 +66,6 @@ class Move_Helper: P_Selectable_Mode {
         lineBelowOpen = true
         lineAboveOpen = true
     }
-    
-//    func movement_With_Single_Note_Selected(){
-//        if let lclNote_Low_Index = note_Low_Index, let lclNote_High_Index = note_High_Index
-//        ,let lclSnapshot_X = snapshot_Cursor_X
-//        {
-//
-//        let delta_X_Grid_Units = parentCentralState.curr_Data_Pos_X - lclSnapshot_X
-//
-//        let proposedNewMinIndex = lclNote_Low_Index + delta_X_Grid_Units
-//        let proposedNewMaxIndex = lclNote_High_Index + delta_X_Grid_Units
-//
-//        if proposedNewMinIndex >= currLeftLimit && proposedNewMaxIndex <= currRightLimit {
-//            proposedSet = parentCentralState.currLineSet
-//            .filter{$0.dataCell_X_Number >= proposedNewMinIndex && $0.dataCell_X_Number <= proposedNewMaxIndex}
-//        }
-//        else if proposedNewMinIndex < currLeftLimit {
-//            proposedSet = parentCentralState.currLineSet
-//            .filter{$0.dataCell_X_Number >= currLeftLimit && $0.dataCell_X_Number <= (lclNote_High_Index - lclNote_Low_Index)}
-//        }
-//        else if proposedNewMaxIndex > currRightLimit {
-//            proposedSet = parentCentralState.currLineSet
-//            .filter{$0.dataCell_X_Number >=  currRightLimit-(lclNote_High_Index - lclNote_Low_Index)
-//                && $0.dataCell_X_Number <= currRightLimit
-//            }
-//        }
-//
-//        potential_Moved_Set = proposedSet
-//
-//        prohibition_Indicator_Set = proposedSet.filter({$0.note_Im_In != nil})
-//
-//        }
-//
-//    }
-
     
     func movement_With_Multi_Note_Selected(){
 
@@ -189,55 +110,30 @@ class Move_Helper: P_Selectable_Mode {
     
     
     func nil_Cell_Sets(){
-
-//        if note_Low_Index != nil{note_Low_Index = nil}
-//
-//        if note_High_Index != nil{note_High_Index = nil}
-//
-//        if snapshot_Cursor_X != nil{snapshot_Cursor_X = nil}
-//
-//        if snapShot_Note_Id_Param != nil{snapShot_Note_Id_Param = nil}
-        
-        // come back to this laters
+        if let lclNoteCollection = parentCentralState.currentNoteCollection{
+            for note in lclNoteCollection.noteArray{
+                if note.note_Is_MultiSelected == true{print("note: ",note.id," is in a multiselect")}
+            }
+        }
         moving_Cell_Set_Holder_Array.removeAll()
-
-//        if proposedSet.count > 0 {
-//            proposedSet.removeAll()
-//        }
-//        if prohibition_Indicator_Set.count > 0 {
-//            prohibition_Indicator_Set.removeAll()
-//        }
-//        if potential_Moved_Set.count > 0 {
-//            potential_Moved_Set.removeAll()
-//        }
-
     }
     
     func writeMovedNote_DeleteOldNote(){
-        //for note_Snapshot in noteSnapShotArray {
-        
         for moving_Cell_Set in moving_Cell_Set_Holder_Array{
             if moving_Cell_Set.potential_Moved_Set.count > 0{
-                            
                 if let currNoteCollection = parentCentralState.currentNoteCollection {
-                    
                     currNoteCollection.delete_Note_By_Id(note_Id_Param: moving_Cell_Set.initial_Snapshot.snapShot_Note_Id_Param)
-                                                            //note_Snapshot.snapShot_Note_Id_Param)
-                    
-                    //for cell in potential_Moved_Set {
                     for cell in moving_Cell_Set.potential_Moved_Set{
                         cell.handleVisibleStateChange(type: .deActivate_Potential_Set)
                     }
-                    
-                    //currNoteCollection.write_Note_Data(cellSetParam: potential_Moved_Set)
                     currNoteCollection.write_Note_Data(cellSetParam: moving_Cell_Set.potential_Moved_Set)
+                    // highlight the sucker in here?
+                    
                 }
-                nil_Cell_Sets()
+                
             }
         }
-        
-            
-        //}
+        nil_Cell_Sets()
     }
     
 }
