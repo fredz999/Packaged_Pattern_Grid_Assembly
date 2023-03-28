8d5207493c6375cf88a504db973af8fdf28a17b4
fredz999 <jjjj58@hotmail.com>
2023-03-28 16:51:03 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper.swift
@@ -58,7 +58,7 @@ class Resize_Helper: P_Selectable_Mode {
             }
         }
         else if mode_Active == true, resizeMode == .leftSideResize {
-            resizeMode = .deactivateResize
+            //resizeMode = .deactivateResize
             parentCentralState.setPatternMode(patternModeParam: .passive_Mode)
         }
         //return generateModeDescriptorString()
