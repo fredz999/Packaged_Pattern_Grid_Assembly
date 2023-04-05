ecafa65f26e851adce84d0719c8a7f3f56462a23
fredz999 <jjjj58@hotmail.com>
2023-04-05 14:56:36 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper.swift
@@ -166,7 +166,7 @@ public class Resize_Helper: ObservableObject, P_Selectable_Mode {
                     snapshot_Note_Cells_Right_Of_Note_Set = snapshot_Cells_Right_Of_Note_Set.filter{$0.note_Im_In != nil}
 
                     if snapshot_Cells_Right_Of_Note_Set.count == 0 {
-                         rightDataXLimit = 0
+                        rightDataXLimit = dimensions.dataGrid_X_Unit_Count-1
                     }
                     else if let minNoteCellRightOfNote = snapshot_Note_Cells_Right_Of_Note_Set.min(by: { $0.dataCell_X_Number < $1.dataCell_X_Number }){
                         rightDataXLimit = minNoteCellRightOfNote.dataCell_X_Number
