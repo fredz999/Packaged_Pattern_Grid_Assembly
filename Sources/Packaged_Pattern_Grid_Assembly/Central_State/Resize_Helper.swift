94b87489ce07ae8e7c4b1651973818a9501354b5
fredz999 <jjjj58@hotmail.com>
2023-04-11 11:34:09 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper.swift
@@ -438,7 +438,7 @@ class Right_Side_Resizer_Garage {
     }
     
     func paintCells(){
-        print("paintCells()")
+        print("paintCells(),new_Note_Cell_Set count: ",new_Note_Cell_Set.count,", available count: ",available_Cell_Set.count)
         for cell in available_Cell_Set {
             cell.reset_To_Original()
             if cell.in_Resize_Set == true {
