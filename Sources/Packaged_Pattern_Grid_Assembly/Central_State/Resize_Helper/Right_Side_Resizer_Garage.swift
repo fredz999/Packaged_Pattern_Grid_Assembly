9a5cb7511cc17f2ba5ab7426d1a3bf3a16a1224f
fredz999 <jjjj58@hotmail.com>
2023-04-13 09:29:30 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper/Right_Side_Resizer_Garage.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper/Right_Side_Resizer_Garage.swift
@@ -63,7 +63,7 @@ class Right_Side_Resizer_Garage {
             if currentHalfCellIndexParam >= lcl_minHalfCellIndex {
                 available_Cell_Set = snapshot_Line_Set.filter{$0.dataCell_X_Number < lcl_RightwardBarrierDataX &&  $0.dataCell_X_Number > lcl_LeftwardBarrierDataX}
                     //.filter{$0.dataCell_X_Number < lcl_RightwardBarrierDataX &&  $0.four_Four_Half_Cell_Index >= lcl_minHalfCellIndex}
-                
+                print("lcl_LeftwardBarrierDataX: ",lcl_LeftwardBarrierDataX.description)
                 new_Note_Cell_Set = available_Cell_Set.filter{$0.four_Four_Half_Cell_Index <= currentHalfCellIndexParam && $0.dataCell_X_Number >= lcl_LeftwardBarrierDataX}
                     //.filter{$0.four_Four_Half_Cell_Index <= currentHalfCellIndexParam && $0.four_Four_Half_Cell_Index >= lcl_minHalfCellIndex}
             }
