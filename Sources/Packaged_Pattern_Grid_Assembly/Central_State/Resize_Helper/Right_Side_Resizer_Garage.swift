4a5e450966430c73f694bd578f8308febd21da8c
fredz999 <jjjj58@hotmail.com>
2023-04-15 15:41:14 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper/Right_Side_Resizer_Garage.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Resize_Helper/Right_Side_Resizer_Garage.swift
@@ -54,15 +54,15 @@ class Right_Side_Resizer_Garage {
             let currentHalfCellIndexParam = lclSnapshotMaxHalfCellIndex + halfCellDeltaParam
             
             if currentHalfCellIndexParam < lcl_minHalfCellIndex {
-                if let lclModifiableNoteData = lclNoteRef.modifiable_Note_Data{
+                if let lclModifiableNoteData = lclNoteRef.modifiable_Note_Data {
                     new_Note_Cell_Set = lclModifiableNoteData.minimumSet
                     if let lastMinimalCell = lclModifiableNoteData.minimumSet.max(by: {$0.dataCell_X_Number < $1.dataCell_X_Number}){
                     available_Cell_Set = snapshot_Line_Set.filter{$0.dataCell_X_Number < lcl_RightwardBarrierDataX &&  $0.dataCell_X_Number > lastMinimalCell.dataCell_X_Number}
                     }
                 }
-                
             }
             if currentHalfCellIndexParam >= lcl_minHalfCellIndex {
+                print("currentHalfCellIndexParam >= lcl_minHalfCellIndex")
                 available_Cell_Set = snapshot_Line_Set.filter{$0.dataCell_X_Number < lcl_RightwardBarrierDataX &&  $0.dataCell_X_Number >= lcl_LeftwardBarrierDataX}
                 new_Note_Cell_Set = available_Cell_Set.filter{$0.four_Four_Half_Cell_Index <= currentHalfCellIndexParam}
             }
