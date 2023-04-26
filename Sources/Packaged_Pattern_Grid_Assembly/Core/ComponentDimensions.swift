b4d4f65d631c89284a7ef3031a5d637b05c0c78a
fredz999 <jjjj58@hotmail.com>
2023-04-26 18:23:22 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Core/ComponentDimensions.swift
@@ -106,7 +106,7 @@ public class ComponentColors {
     
     public var multiSelectedNoteColor : Color = Color(red:1,green:1,blue:0)
     
-    public var selectedNoteColor : Color = Color(red:0.7,green:0,blue:0)
+    public var selectedNoteColor : Color = Color(red:0.5,green:1,blue:0)
     
     public var prohibited_Cell_Color : Color = Color(red:0.6,green:0,blue:0)
     
