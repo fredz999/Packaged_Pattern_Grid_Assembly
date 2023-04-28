9d9e893622aa6655621eab2bdfdceab044a80f08
fredz999 <jjjj58@hotmail.com>
2023-04-28 18:43:40 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -202,12 +202,14 @@ class Moving_Cell_Set_Holder {
             let delta = potential_Moved_Set.symmetricDifference(newValue)
             for cell in delta {
                 cell.handleVisibleStateChange(type: .deActivate_Potential_Set)
+                cell.handleVisibleStateChange(type: .deActivate_Prohibited_Moving_Cell)
             }
         }
         didSet {
             if noteImIn.parent_Note_Collection.parentCentralState.movingNoteCurrentlyWriteable == true {
                 for cell in potential_Moved_Set {
                     cell.handleVisibleStateChange(type : .activate_Potential_Set)
+                    cell.handleVisibleStateChange(type: .activate_Prohibited_Moving_Cell)
                 }
             }
         }
@@ -236,7 +238,7 @@ class Moving_Cell_Set_Holder {
             }
         }
     }
-    
+    // TODO: figure out how to get
     func handleNoteWriteabilityChange(noteWriteable:Bool){
         if noteWriteable == true {
             for cell in potential_Moved_Set {
