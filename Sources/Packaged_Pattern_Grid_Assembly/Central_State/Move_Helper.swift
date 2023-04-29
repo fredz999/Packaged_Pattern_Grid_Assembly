51541db0daab6e18b0a9151f6f7442ff3645a607
fredz999 <jjjj58@hotmail.com>
2023-04-29 17:22:44 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -262,8 +262,8 @@ class Moving_Cell_Set_Holder {
                 for cell in potential_Moved_Set {
                     if cell.in_Prohibited_Moving_Cell_Set == false {
                         cell.handleVisibleStateChange(type: .activate_Prohibited_Moving_Cell)
+                        print("cell x: ", cell.dataCell_X_Number)
                     }
-                    //print("cell x: ", cell.dataCell_X_Number)
                     //print(".in_Prohibited_Moving_Cell_Set == true ? : ",cell.in_Prohibited_Moving_Cell_Set.description,", noteId: ",noteImIn.id)
                 }
             }
