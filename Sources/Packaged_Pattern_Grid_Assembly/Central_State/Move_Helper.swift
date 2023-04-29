9e15f3e7866f4dc80d705b8259d04f14ff087561
fredz999 <jjjj58@hotmail.com>
2023-04-29 17:21:24 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper.swift
@@ -258,9 +258,12 @@ class Moving_Cell_Set_Holder {
                 }
             }
             else if noteImIn.movingNoteCurrentlyWriteable == false {
+                
                 for cell in potential_Moved_Set {
-                    cell.handleVisibleStateChange(type: .activate_Prohibited_Moving_Cell)
-                    print("cell x: ", cell.dataCell_X_Number)
+                    if cell.in_Prohibited_Moving_Cell_Set == false {
+                        cell.handleVisibleStateChange(type: .activate_Prohibited_Moving_Cell)
+                    }
+                    //print("cell x: ", cell.dataCell_X_Number)
                     //print(".in_Prohibited_Moving_Cell_Set == true ? : ",cell.in_Prohibited_Moving_Cell_Set.description,", noteId: ",noteImIn.id)
                 }
             }
