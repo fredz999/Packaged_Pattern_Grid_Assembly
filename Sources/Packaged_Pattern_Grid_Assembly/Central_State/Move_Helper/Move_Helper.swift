6fa7669da06b84b5bb50e32db26ff97f234cf341
fredz999 <jjjj58@hotmail.com>
2023-04-30 15:28:48 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper/Move_Helper.swift
@@ -118,8 +118,7 @@ class Move_Helper: P_Selectable_Mode {
                 }
                 moving_Cell_Set_Holder_Array[m].potential_Moved_Set = proposedSet
                 moving_Cell_Set_Holder_Array[m].prohibition_Indicator_Set = moving_Cell_Set_Holder_Array[m]
-                .potential_Moved_Set.filter({$0.note_Im_In != moving_Cell_Set_Holder_Array[m].noteImIn}) //noteImIn
-                    //.potential_Moved_Set.filter({$0.note_Im_In != nil})
+                .potential_Moved_Set.filter({$0.note_Im_In != nil})
             }
         }
     }
