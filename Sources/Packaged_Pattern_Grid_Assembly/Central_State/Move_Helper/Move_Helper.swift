ace1658bbef66dc33b87650e4bd271a74a8d55a7
fredz999 <jjjj58@hotmail.com>
2023-05-01 12:58:48 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper/Move_Helper.swift
@@ -186,7 +186,7 @@ class Move_Helper: P_Selectable_Mode {
             }
             else if dont_Copy_Just_Move == false {
                 for moving_Cell_Set in moving_Cell_Set_Holder_Array {
-                    moving_Cell_Set.noteImIn.moved_Away_From = false
+                    //moving_Cell_Set.noteImIn.moved_Away_From = false
                     if moving_Cell_Set.movingNoteCurrentlyWriteable == true {
                         for cell in moving_Cell_Set.potential_Moved_Set {
                             cell.handleVisibleStateChange(type: .deActivate_Potential_Set)
