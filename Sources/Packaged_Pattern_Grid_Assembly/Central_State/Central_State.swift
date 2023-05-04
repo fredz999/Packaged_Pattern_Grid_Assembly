73f7bcb5fc6a27a9cd8366e02ca26f9dd51c9d1c
fredz999 <jjjj58@hotmail.com>
2023-05-04 22:36:40 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Central_State.swift
@@ -376,6 +376,7 @@ public class Central_State : ObservableObject {
             curr_Data_Pos_Y = currentYCursor_Slider_Position + lower_Bracket_Number
             centralState_Data_Evaluation()
             print("allo allo:",alloCount.description)
+            alloCount+=1
         }
     }
 
