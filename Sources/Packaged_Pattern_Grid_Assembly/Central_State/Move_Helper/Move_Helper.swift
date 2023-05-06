b0a360f672456967806f66e9d68e388e2a1bdfac
fredz999 <jjjj58@hotmail.com>
2023-05-06 14:57:51 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper/Move_Helper.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_State/Move_Helper/Move_Helper.swift
@@ -144,29 +144,15 @@ class Move_Helper: P_Selectable_Mode {
                     && $0.dataCell_X_Number <= currRightLimit_Move
                     }
                 }
-                
-                
-                
-                
                 if dont_Copy_Just_Move == true {
                     moving_Cell_Set_Holder_Array[m].potential_Moved_Set = proposedSet
                     let clashedSet = proposedSet.filter({$0.note_Im_In != moving_Cell_Set_Holder_Array[m].noteImIn && $0.note_Im_In != nil})
                     moving_Cell_Set_Holder_Array[m].prohibition_Indicator_Set = clashedSet
                 }
                 else if dont_Copy_Just_Move == false {
-                      print("what about in here?: 0")
-                    
-//                    moving_Cell_Set_Holder_Array[m].potential_Moved_Set = proposedSet
-//                    moving_Cell_Set_Holder_Array[m].prohibition_Indicator_Set = moving_Cell_Set_Holder_Array[m]
-//                    .potential_Moved_Set.filter({$0.note_Im_In != nil})
-                    
                     moving_Cell_Set_Holder_Array[m].prohibition_Indicator_Set = proposedSet.filter({$0.note_Im_In != nil})
                     moving_Cell_Set_Holder_Array[m].potential_Moved_Set = proposedSet.subtracting(moving_Cell_Set_Holder_Array[m].prohibition_Indicator_Set)
-                    
                 }
-                
-                
-                
             }
         }
     }
