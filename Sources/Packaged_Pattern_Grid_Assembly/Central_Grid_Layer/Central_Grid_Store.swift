0abf6c5d8438d05d8714588d82ed1f628c457cef
fredz999 <jjjj58@hotmail.com>
2023-05-07 19:23:29 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Central_Grid_Store.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Central_Grid_Layer/Central_Grid_Store.swift
@@ -113,7 +113,7 @@ public class Central_Cell_Store : ObservableObject,Identifiable, Equatable, Hash
         // && the cell hasnt got the state change thats being done here
         
         if new_Data_Cell.note_Im_In != nil  {
-            //print("alterCursorState call 1", ", YNum: ",new_Data_Cell.parentLine.line_Y_Num)
+            print("alterCursorState call 1", ", YNum: ",new_Data_Cell.parentLine.line_Y_Num)
             //new_Data_Cell.alterCursorState(thisCellInNote: true, cell_In_Moving_Prohibition: nil, cell_In_Clashing_Prohibition: nil, cursorMoveMode: nil)
         }
         
