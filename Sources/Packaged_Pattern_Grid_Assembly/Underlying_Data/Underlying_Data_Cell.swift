c49e314b98704104a951227fe2b30560d15081c0
fredz999 <jjjj58@hotmail.com>
2023-05-09 17:18:46 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Cell.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Cell.swift
@@ -273,11 +273,17 @@ public class Underlying_Data_Cell:Identifiable,Equatable,Hashable {
             if currentType == .start_Blank || currentType == .mid_Blank || currentType == .end_Blank{
                 change_Type(newType: fourStatus)
             }
+            else {
+                print("else at least triggerred,timing = 44")
+            }
             note_Reset_Status = fourStatus
         }
         else if timingParam == .sixEight {
             if currentType == .start_Blank || currentType == .mid_Blank || currentType == .end_Blank{
                 change_Type(newType: sixStatus)
+            }
+            else{
+                print("else at least triggerred, timing = 68")
             }
             note_Reset_Status = sixStatus
         }
