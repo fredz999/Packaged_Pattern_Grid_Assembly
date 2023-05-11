de915b0b572cebc852a543ced9c9d7440fd9ad2c
fredz999 <jjjj58@hotmail.com>
2023-05-11 12:26:24 +0100

    _

--- Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Cell.swift
+++ Sources/Packaged_Pattern_Grid_Assembly/Underlying_Data/Underlying_Data_Cell.swift
@@ -322,7 +322,8 @@ public class Underlying_Data_Cell:Identifiable,Equatable,Hashable {
         
         
         if currentType != newType {
-            if dataCell_X_Number < 6 && parentLine.line_Y_Num == 24{
+            if dataCell_X_Number < 6 && parentLine.line_Y_Num == 24
+            || parentLine.line_Y_Num == 0{
                 print("cellX: ",dataCell_X_Number , ",Y: ",parentLine.line_Y_Num, ", Cell func, startType: ",currentType.rawValue,", newType: ",newType)
             }
             
